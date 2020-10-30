b61619ba01eececebc4ee2749c68773a7aaa6135
Ethan Baruh <baruh.ethan@gmail.com>
2020-10-30 12:27:19 -0700

    canvas drawing complete

--- digit_recog/draw.js
+++ digit_recog/draw.js
@@ -2,16 +2,50 @@
 var canvas = document.getElementById('mnist_canvas');
 var ctx = canvas.getContext('2d');
 
-canvas.addEventListener("mousedown", function(e) {
-   // Get x, y coordinates inside of the canavs so we can start to draw
-   var x = e.clientX - canvas.offsetLeft;
-   var y = e.clientY - canvas.offsetTop;
+// Initialize variables
+var flag = false;
+var x, y;
 
-   // Prepare the context to begin drawing
-   ctx.moveTo(x, y);
-   ctx.beginPath();
-});
+canvas.addEventListener("mousedown", function(e) {draw("down", e)});
 
-canvas.addEventListener("mousemove", function(e) {
-   // TODO: Add functionality here
-});
+canvas.addEventListener("mousemove", function(e) {draw("move", e)});
+
+canvas.addEventListener("mouseup", function (e) {draw("up", e)})
+
+// Draws shape for img input
+function draw(res, e) {
+   // On mousedown
+   if (res == "down") {
+
+      // Get x, y coordinates inside of the canavs so we can start to draw
+      x = e.clientX - canvas.offsetLeft;
+      y = e.clientY - canvas.offsetTop;
+
+      // So we know to start drawing and to keep drawing until flag = false
+      flag = true;
+      ctx.moveTo(x, y);
+      ctx.beginPath();
+   }
+   // On mousemove
+   else if (res == "move") {
+      if (flag) {
+         // Get x, y coordinates inside of the canavs so we can start to draw
+         x = e.clientX - canvas.offsetLeft;
+         y = e.clientY - canvas.offsetTop;
+
+         // Create a line to the mouse's coordinates and fill it
+         ctx.lineTo(x, y);
+         ctx.stroke();
+      }
+   }
+   // On mouseup
+   else if (res == "up") {
+      // Set flag to false so the canvas will stop drawing
+      flag = false;
+   }
+}
+
+// Clear rectangle
+function clearCanvas() {
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
