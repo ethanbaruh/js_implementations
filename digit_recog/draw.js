7290613a1b73c93f14bf3653a3f0e968d8863398
Ethan Baruh <baruh.ethan@gmail.com>
2020-10-30 11:33:23 -0700

    small stuff

--- digit_recog/draw.js
+++ digit_recog/draw.js
@@ -1,11 +1,13 @@
+// Initialize canvas DOM and create the context
 var canvas = document.getElementById('mnist_canvas');
 var ctx = canvas.getContext('2d');
-window.alert(canvas.offsetTop);
 
 canvas.addEventListener("mousedown", function(e) {
+   // Get x, y coordinates inside of the canavs so we can start to draw
    var x = e.clientX - canvas.offsetLeft;
    var y = e.clientY - canvas.offsetTop;
 
+   // Prepare the context to begin drawing
    ctx.moveTo(x, y);
    ctx.beginPath();
 });
